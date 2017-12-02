a111c9ed56a72a8e01567a534a88e1c74b7c5957
markonijum <markonijum@gmail.com>
2017-12-02 19:13:59 +0100

    Adding bootstrap framework

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,9 +1,7 @@
 import $ from 'jquery';
 import whatInput from 'what-input';
-
-
 window.$ = $;
-
+// import 'bootstrap-sass/assets/javascripts/bootstrap';
 import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
